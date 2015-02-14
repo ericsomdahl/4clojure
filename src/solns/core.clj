6c84094237da452f1ef5c2abbf98a9103558a2cd
Eric Somdahl <ericsomdahl@gmail.com>
2015-02-14 14:20:08 -0500

    new solutions

--- src/solns/core.clj
+++ src/solns/core.clj
@@ -5,4 +5,14 @@
 ;;; Nothing but the Truth
 (= true true)
 
+;;; 2
+;;; Simple Math
+(=  (- 10  (* 2 3)) 4)
 
+;;; 3
+;;; Intro to Strings
+(= "HELLO WORLD" (.toUpperCase "hello world"))
+
+;;; 4
+;;; Intro to Lists
+(=  (list :a :b :c) '(:a :b :c))
