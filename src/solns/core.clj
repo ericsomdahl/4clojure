2013aec0a4326315a7cf5a9ca137aa536c8d426e
Eric Somdahl <ericsomdahl@gmail.com>
2015-02-20 19:39:36 -0500

    drop nth

--- src/solns/core.clj
+++ src/solns/core.clj
@@ -317,5 +317,23 @@
 (def forty
   (fn [x y] (butlast (mapcat #(list % x) y))))
 
+;;; 41
+;;; Drop every nth item
+
+(def forty-one
+  (fn [x y]
+    (mapcat #(if (= y (count %))
+                (butlast %)
+                %)
+            (partition-all y x))))
+
+(= (forty-one [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8])
+
+
+
+
+
+
+
 
 
